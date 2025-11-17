744c9645a0f96c8ee792e975c3f1038275efd9c5
Joost Molenaar <jjm@j0057.nl>
2025-11-17 16:19:08 +0100

    Fix brew audit new pick
    Closes #3.
    
    Signed-off-by: Joost Molenaar <jjm@j0057.nl>

--- Formula/git-credential-azure.rb
+++ Formula/git-credential-azure.rb
@@ -5,12 +5,12 @@ class GitCredentialAzure < Formula
   sha256 "1f3cd884d110e10ebd1e166f767df6da2981b51db3692a9fe33a53df83376bbf"
   license "Apache-2.0"
 
-  depends_on "go" => :build
-
   bottle do
     rebuild 1
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "6478e194ec1d86d1de718e7de3a788eeb6db4c8e8d20d2a11da2e040971df5e6"
   end
+
+  depends_on "go" => :build
 
   def install
     system "go", "build", *std_go_args(ldflags: "-s -w -X main.version=#{version}")
