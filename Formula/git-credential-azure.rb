8e94334a7c1fcd8add2ef6bb4aa7d83eddcc02a8
Joost Molenaar <jjm@j0057.nl>
2025-11-18 13:14:51 +0100

    Hack version number for testing

--- Formula/git-credential-azure.rb
+++ Formula/git-credential-azure.rb
@@ -1,8 +1,8 @@
 class GitCredentialAzure < Formula
   desc "Git credential helper for Azure Repos"
   homepage "https://github.com/hickford/git-credential-azure"
-  url "https://github.com/hickford/git-credential-azure/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "1f3cd884d110e10ebd1e166f767df6da2981b51db3692a9fe33a53df83376bbf"
+  url "https://github.com/hickford/git-credential-azure/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "fefa8e8f011cfc02c86fb9d7178f3d1819eb99ef107aeab877ea550e184b655c"
   license "Apache-2.0"
 
   bottle do
