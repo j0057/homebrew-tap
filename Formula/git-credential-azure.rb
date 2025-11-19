72c6ded7959cc3b2143e68c7946429c081136a64
Joost Molenaar <jjm@j0057.nl>
2025-11-19 18:47:28 +0100

    Bump version to 0.3.1-3

--- Formula/git-credential-azure.rb
+++ Formula/git-credential-azure.rb
@@ -1,13 +1,13 @@
 class GitCredentialAzure < Formula
   desc "Git credential helper for Azure Repos"
   homepage "https://github.com/hickford/git-credential-azure"
-  url "https://github.com/hickford/git-credential-azure/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "fefa8e8f011cfc02c86fb9d7178f3d1819eb99ef107aeab877ea550e184b655c"
+  url "https://github.com/hickford/git-credential-azure/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "1f3cd884d110e10ebd1e166f767df6da2981b51db3692a9fe33a53df83376bbf"
   license "Apache-2.0"
 
   bottle do
     root_url "https://ghcr.io/v2/j0057/tap"
-    rebuild 2
+    rebuild 3
     sha256 cellar: :any_skip_relocation, arm64_tahoe:   "19045d3046532b9783df98689ec6d12aa8951337b7ccbcc81a81c931b56c3c0b"
     sha256 cellar: :any_skip_relocation, arm64_sequoia: "e51ea4e163562a3e1ce0d3c8073940d841099d01eee4d44d286436c2f0521368"
     sha256 cellar: :any_skip_relocation, arm64_sonoma:  "66a257d25a0573834316cb81113f354c5f964abe2abac1936b6a36d0f34e42a5"
